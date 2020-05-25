42a752a22d82e885750c52fd7a8a6ab92e0484f0
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-05-24 18:25:19 -0700

    The licensing

--- Cross-Platform/Free Space FAT32 zero-wipe/free-space-fat32-zero-wipe.sh
+++ Cross-Platform/Free Space FAT32 zero-wipe/free-space-fat32-zero-wipe.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Takes an already FAT32 formatted drive, and clears up it's free space.
+# Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 PROMPT_COMMAND='echo -en "\033]0;Free Space FAT32 Drive Wipe\a"'
 if [[ -v WSLENV ]]; then is_wsl=false; else is_wsl=true; fi
 
