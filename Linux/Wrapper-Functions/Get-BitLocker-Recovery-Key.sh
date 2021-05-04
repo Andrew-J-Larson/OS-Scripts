da301f9c2163d6cb4ed3724d824fd05981962666
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-05-04 15:05:13 -0700

    Create Get-BitLocker-Recovery-Key.sh
    Allows using bash to get the BitLocker recovery key for a computer (needs key identifier, and credentials passed in)

---
+++ Linux/Wrapper-Functions/Get-BitLocker-Recovery-Key.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Only tested on Ubuntu
+# Requires curl to be installed
+
+# Constants
+MBAM_URL="https://mbam.example.com" # the MS bitlocker website for your organization
+MBAM_Admin_KeyRecoveryURL="${MBAM_URL}/HelpDesk/KeyRecoveryPage.aspx" # the page needed to get recovery information
+COOKIES_FILE=/tmp/Cookies.txt
+RESULTS_FILE=/tmp/Results.html
+DECODED_FILE=/tmp/Decoded.txt
+
+# functions to parse xml documents
+xmlgetnext () {
+  local IFS='>'
+  read -d '<' TAG VALUE
+}
+parseXML () {
+  cat "$1" | while xmlgetnext ; do echo "$TAG" ; done
+}
+# function to encode data for form submit
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"
+}
+
+# function for getting BitLocker recovery keys
+# returns nothing if it couldn't find key
+getBdeRecoveryKey () {
+  bdeKeyID="$1" # this is the BDE key identifier
+  usr="$2" # this is the username
+  psswrd="$3" # this is the password
+  # first curl to login, and get: the SessionID (from COOKIE_FILE) and the ViewState/EventValidation (from DECODED_FILE)
+  curl --ntlm --user "${usr}:${psswrd}" -c "${COOKIES_FILE}" "${MBAM_Admin_KeyRecoveryURL}" -o "${RESULTS_FILE}" -s
+  # capture and encode data to send back in form
+  SessionID=$(cat "${COOKIES_FILE}" | tail -1 | awk '{print $7}')
+  parseXML "${RESULTS_FILE}" > "${DECODED_FILE}"
+  # form won't be valid unless it has data from webpage also submitted with it
+  ViewState=$(rawurlencode $(cat "${DECODED_FILE}" | grep '"__VIEWSTATE"' | grep -oP '(?<=value=").*?(?=")'))
+  EventValidation=$(rawurlencode $(cat "${DECODED_FILE}" | grep '"__EVENTVALIDATION"' | grep -oP '(?<=value=").*?(?=")'))
+  DataRaw="__LASTFOCUS=&__EVENTTARGET=ctl00%24content%24SubmitButton&__EVENTARGUMENT=&__VIEWSTATE=${ViewState}&__VIEWSTATEGENERATOR=C0534C36&__EVENTVALIDATION=${EventValidation}&ctl00%24content%24DomainNameTextBox=&ctl00%24content%24UserNameTextBox=&ctl00%24content%24KeyIdTextBox=${bdeKeyID}&ctl00%24content%24ReasonCodeSelect=Other"
+  # second curl to send form, and get data
+  curl -L --ntlm --user "${usr}:${psswrd}" "${MBAM_Admin_KeyRecoveryURL}" -o "${RESULTS_FILE}" -s \
+    -H 'Connection: keep-alive' \
+    -H 'Cache-Control: max-age=0' \
+    -H "Origin: ${MBAM_Admin_KeyRecoveryURL}" \
+    -H 'Upgrade-Insecure-Requests: 1' \
+    -H 'DNT: 1' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+    -H 'Sec-Fetch-Site: same-origin' \
+    -H 'Sec-Fetch-Mode: navigate' \
+    -H 'Sec-Fetch-User: ?1' \
+    -H 'Sec-Fetch-Dest: document' \
+    -H "Referer: ${MBAM_Admin_KeyRecoveryURL}" \
+    -H 'Accept-Language: en-US,en;q=0.9' \
+    -H "Cookie: ASP.NET_SessionId=${SessionID}" \
+    --data-raw "${DataRaw}" \
+    --compressed
+  # capture and use bitlocker key
+  parseXML "${RESULTS_FILE}" > "${DECODED_FILE}"
+  BitLockerKey=$(cat "${DECODED_FILE}" | grep '"ctl00$content$KeyReturnField"' | grep -oP '(?<=value=").*?(?=")')
+  
+  # remove temp files
+  rm "${COOKIES_FILE}" "${RESULTS_FILE}" "${DECODED_FILE}"
+  
+  # Results
+  echo "$BitLockerKey"
+  
+  # clearing stored password for security reasons
+  psswrd=""
+}
