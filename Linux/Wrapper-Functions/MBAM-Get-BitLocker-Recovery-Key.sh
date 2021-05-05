a8bc948fdfd5c024c9194c11dee23b1b5c2184a6
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-05-05 11:29:42 -0700

    Added license short info

--- Linux/Wrapper-Functions/MBAM-Get-BitLocker-Recovery-Key.sh
+++ Linux/Wrapper-Functions/MBAM-Get-BitLocker-Recovery-Key.sh
@@ -2,6 +2,21 @@
 # Only tested on Ubuntu
 # Requires curl to be installed
 
+# Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Constants
 MBAM_URL="https://mbam.example.com" # the MBAM website for your organization
 MBAM_Admin_KeyRecoveryURL="${MBAM_URL}/HelpDesk/KeyRecoveryPage.aspx" # the page needed to get recovery information
