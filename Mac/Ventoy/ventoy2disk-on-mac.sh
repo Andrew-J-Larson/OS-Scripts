94a57fe167bf1f4f799fee386e1ee6836c1240d4
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-05-26 10:33:22 -0700

    Create ventoy2disk-on-mac.sh

---
+++ Mac/Ventoy/ventoy2disk-on-mac.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# This script will automatically download and run qemu (since it is portable) and create a VM to run
+# Ventoy LiveCD, simulating the same result of the `ventoy2disk.sh` script on Linux (which isn't compatible
+# with MacOS at the time of writing this, 5/26/2021).
+
+# Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# TODO: Everything
