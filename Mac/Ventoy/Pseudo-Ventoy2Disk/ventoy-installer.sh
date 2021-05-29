d97a075d8aeb1a72929c035b0d0c6bf349dab2c4
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-05-28 17:25:03 -0700

    More code in
    - Won't start as root (since it needs the user profile)
    - Checks for internet connection
    - Correctly starts in the zsh or bash shell depending on which one is available
    - Gets folder selection from user if installing to folder

--- Mac/Ventoy/Pseudo-Ventoy2Disk/ventoy-installer.sh
+++ Mac/Ventoy/Pseudo-Ventoy2Disk/ventoy-installer.sh
@@ -26,12 +26,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# Conditionally start script in zsh or bash
+if [ "$SHELL" = "/bin/sh" ]; then
+  # preferably start in zsh
+  shell=
+  if [ -f /bin/zsh ]; then
+    shell=/bin/zsh
+  elif [ -f /bin/bash ]; then
+    shell=/bin/bash
+  fi
+
+  # execute in shell or warn that we can't execute
+  if [ -n "$shell" ]; then
+    exec $shell "$0"
+    exit
+  else
+    printf "Can't execute script, can't find a required shell environment:\n - zsh (missing)\n - bash (missing)\n"
+    exit 1
+  fi
+fi
+
 # CONSTANTS
 Xcode_Install="/Applications/Xcode.app"
 Xcode_App_URL="https://apps.apple.com/app/id497799835"
+MacPorts_Releases="https://api.github.com/repos/macports/macports-base/releases"
 QEMU_Releases="https://api.github.com/repos/qemu/qemu/releases"
 Ventoy_Releases="https://api.github.com/repos/ventoy/Ventoy/releases"
-User_Install="~/Ventoy"
 # TODO: any other constants needed
 
 # VARIABLES
@@ -44,19 +64,19 @@ folderInstall= # gets set to choosen directory if installing to folder
 # Ensure that we are running on a supported MacOS system
 if [ "$(uname -s)" = "Darwin" ]; then
   macVersion="$(sw_vers -productVersion)"
-  majVer="$(echo "${macVersion}" | cut -d'.' -f1)"
-  minVer="$(echo "${macVersion}" | cut -d'.' -f2)"
-  if [ -z "${minVer}" ]; then minVer="0"; fi
+  majVer="$(echo "$macVersion" | cut -d'.' -f1)"
+  minVer="$(echo "$macVersion" | cut -d'.' -f2)"
+  if [ -z "$minVer" ]; then minVer="0"; fi
 
   # check version: QEMU is only supported on 10.5+,
   # but I'm only going to support the top 3 major Mac versions
-  if [ "${majVer}" -le 10 ]; then
+  if [ "$majVer" -le 10 ]; then
     unsupported=0
-    if [ "${majVer}" -eq 10 ] && [ "${minVer}" -lt 14 ]; then unsupported=1; fi
-    if [ "${majVer}" -lt 10 ]; then unsupported=1; fi
+    if [ "$majVer" -eq 10 ] && [ "$minVer" -lt 14 ]; then unsupported=1; fi
+    if [ "$majVer" -lt 10 ]; then unsupported=1; fi
 
-    if [ "${unsupported}" -eq 1 ]; then
-      echo "Sorry, but this version of MacOS (${macVersion}) is not supported."
+    if [ "$unsupported" -eq 1 ]; then
+      echo "Sorry, but this version of MacOS ($macVersion) is not supported."
       exit 1
     fi
   fi
@@ -65,9 +85,16 @@ else # not a mac
   exit 1
 fi
 
+# We don't want to run as root
+if [ "$EUID" -eq 0 ]; then
+  echo "Please don't run this script as root."
+  exit 1
+fi
+
 # Internet connection required
 connected=1
-case "$(curl -s --max-time 2 -I https://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+echo "Checking internet connection..."
+case "$(curl -s --max-time 4 -I https://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
   [23]) connected=0;;
   5) echo "Sorry, but the web proxy won't let us through.";;
   *) echo "Sorry, but the network is down or very slow.";;
@@ -79,7 +106,7 @@ fi
 
 # Make sure Xcode is installed with CLI tools
 if [ ! -f "${Xcode_Install}" ]; then
-  printf "Xcode isn't installed, but required.\nPlease go to ${Xcode_App_URL} and install the Xcode app.\n\n"
+  echo -e "Xcode isn't installed, but required.\nPlease go to ${Xcode_App_URL} and install the Xcode app.\n"
   read -p "Then, press any key to continue."
 fi # second if is to check if it was installed before the end of the read command
 if [ -f "${Xcode_Install}" ]; then
@@ -123,7 +150,7 @@ else # prompt to install a package manager
   while true; do
     read -p "Would you like to install the [H]omeBrew or [M]acPorts package manager, or [C]ancel installation? " hm
     case ${hm} in
-      [Hh]* ) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; break;;
+      [Hh]* ) $SHELL -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; break;;
       [Mm]* ) echo "Please go to https://www.macports.org/install.php and download/install the package for your system."; break;;
       [Cc]* ) exit 1; break;;
       * ) echo "Please answer with H (HomeBrew) or M (MacPorts), or C (Cancel installation).";;
@@ -155,23 +182,27 @@ else
 fi
 
 # Choose between user or folder install
-installType=
+needQemuPackage=0 # used to optionally prompt to uninstall from package manager when folder install is used
 while true; do
   read -p "Would you like to install to [U]ser account or [F]older, or [C]ancel installation?" uf
   case ${uf} in
-    [Uu]* ) installType="U"; break;;
-    [Ff]* ) installType="F"; break;;
+    [Uu]* ) folderInstall="~/"; break;;
+    [Ff]* ) folderInstall=`/usr/bin/osascript << EOT
+                tell application "Finder"
+                    activate
+                    set folderInstall to choose folder with prompt "Select the folder you want to install Ventoy to"
+                end tell
+                return (posix path of folderInstall)
+EOT`; if [ -z "$folderInstall" ]; then echo "No folder selected, aborted."; exit 1; fi; break;;
     [Cc]* ) exit 1; break;;
     * ) echo "Please answer with U (User account) or F (Folder), or C (Cancel installation).";;
   esac
+  folderInstall+="Ventoy"
 done
-if [ "${installType}" = "U" ]; then
-  # install to user's folder, if not already
-  # TODO: ...
-elif [ "${installType}" = "F" ]; then
-  # install to specific folder (doesn't install QEMU to system, or create VM in user folder)
-  # TODO: ...
-fi
+
+# Check if location already has install
+# confirm all required files are installed
+# TODO: (doesn't install QEMU to system, or create VM in user folder)
 # TODO: (check for user account Ventoy installation first)
 #   - If a user install is detected, prompt for uninstallation or repair
 #   - Locally: Will need to download QEMU source then compile binaries manaually for MacOS (see https://wiki.qemu.org/Hosts/Mac#Building_QEMU_for_macOS),
