300ffd9ea36a8bf44067fdc278711be0c6b1b774
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-06-02 13:18:44 -0700

    Update list-usb-mass-storage-devices.sh

--- Mac/list-usb-mass-storage-devices.sh
+++ Mac/list-usb-mass-storage-devices.sh
@@ -4,8 +4,8 @@
 # - `system_profiler SPUSBDataType`
 # However, it still shows up in `ioreg`, so using some filtering, we can grab the list of
 # the connected USB Mass Storage Devices.
-# Note: Some devices, such as external SD Card readers with more than one slot, will not
-#       show more than the one device (the reader) in the list.
+# Note: Some devices, such as external SD Card readers with more than one slot, will only
+#       show one device (the reader) in the list.
 
 command="ioreg -p IOUSB -c AppleUSBDevice -k uid -w0 -r"
 
