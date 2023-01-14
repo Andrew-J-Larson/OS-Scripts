d6b7abf5b5e2db26cc8b1b274f07fd806c7b39ef
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2023-01-13 17:49:51 -0800

    Fixes + More apps + Feature
    Repositioned arguments, which broke it in some places in script (fixed)
    
    A few more apps, some are OEM, others third party.
    
    Now also including the ability to set comment for shortcuts.

--- Windows/Microsoft-Endpoint-Defender/Recreate-Base-Shortcuts.ps1
+++ Windows/Microsoft-Endpoint-Defender/Recreate-Base-Shortcuts.ps1
@@ -6,31 +6,35 @@
 
 # Application objects are setup like so:
 <# @{
-       Name='[name of shortcut here]';
-       Target='[path to exe here]';
-       Arguments='[any arguments that an app starts with here]';
-       SystemLnk='[path to lnk or name of app here]';
-       StartIn='[start in path if needed, here]'
+       Name="[name of shortcut here]";
+       Target="[path to exe here]";
+       Arguments="[any arguments that an app starts with here]";
+       SystemLnk="[path to lnk or name of app here]";
+       StartIn="[start in path if needed, here]";
+       Description="[comment that shows up in tooltip, here]"
    } #>
 
 function Recreate-Shortcut {
-  Set-Variable ProgramShortcutsPath -Option Constant -Value 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs'
+  Set-Variable ProgramShortcutsPath -Option Constant -Value "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
 
   $sName = $args[0] # Required
-  $sTarget = $args[2] # Required
-  $sArguments = $args[3] # Optional (for special shortcuts)
-  $sSystemLnk = $args[1] # Optional (for if name / path is different from normal)
+  $sTarget = $args[1] # Required
+  $sArguments = $args[2] # Optional (for special shortcuts)
+  $sSystemLnk = $args[3] # Optional (for if name / path is different from normal)
   $sStartIn = $args[4] # Optional (for special shortcuts)
+  $sDescription = $args[5] # Optional (some shortcuts have comments for tooltips)
 
   if (Test-Path $sTarget -PathType leaf) {
-    $WScriptObj = New-Object -ComObject ('WScript.Shell')
+    $WScriptObj = New-Object -ComObject ("WScript.Shell")
 
     # if shortcut path not given, create one at default location with $sName
     if (-Not ($sSystemLnk)) {$sSystemLnk = $sName}
     # if doesn't have $ProgramShortcutsPath path included (and not start with drive letter), it'll assume to create path there
     if (-Not ($sSystemLnk -match '^[a-zA-Z]:\\.*' -Or $sSystemLnk -match ('^'+[Regex]::Escape($ProgramShortcutsPath)+'.*'))) {
-      $sSystemLnk = ($ProgramShortcutsPath+'\'+$sSystemLnk)
+      $sSystemLnk = $ProgramShortcutsPath+'\'+$sSystemLnk
     }
+    # if it ends with '\', then we append the name to the end
+    if ($sSystemLnk.EndsWith('\')) {$sSystemLnk = $sSystemLnk+$sName}
     # if doesn't end with .lnk, add it
     if (-Not ($sSystemLnk -match '.*\.lnk$')) {$sSystemLnk = $sSystemLnk+'.lnk'}
     $newLNK = $WscriptObj.CreateShortcut($sSystemLnk)
@@ -41,6 +45,8 @@ function Recreate-Shortcut {
 
     if ($sStartIn) {$newLNK.WorkingDirectory = $sStartIn}
 
+    if ($sDescription) {$newLNK.Description = $sDescription}
+
     $newLNK.Save()
   }
 }
@@ -48,34 +54,41 @@ function Recreate-Shortcut {
 # System Applications
 
 $sysAppList = @(
-  @{Name='Microsoft Edge'; Target='C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'}
-  @{Name='OneDrive'; Target='C:\Program Files\Microsoft OneDrive\OneDrive.exe'},
-  @{Name='Access'; Target='C:\Program Files\Microsoft Office\root\Office16\MSACCESS.EXE'},
-  @{Name='Excel'; Target='C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE'},
-  @{Name='OneNote'; Target='C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE'},
-  @{Name='Outlook'; Target='C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE'},
-  @{Name='PowerPoint'; Target='C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE'},
-  @{Name='Publisher'; Target='C:\Program Files\Microsoft Office\root\Office16\MSPUB.EXE'},
-  @{Name='Word'; Target='C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE'}#,
-#  @{Name=''; Target=''},
-#  @{Name=''; Target=''; Arguments=''; SystemLnk=''; StartIn=''}
+  @{Name="Microsoft Edge"; Target="C:\Program Files\Microsoft\Edge\Application\msedge.exe"} # it's the only install on 32-bit
+  @{Name="Microsoft Edge"; Target="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"} # it's the only install on 64-bit
+  @{Name="OneDrive"; Target="C:\Program Files\Microsoft OneDrive\OneDrive.exe"},
+  @{Name="Access"; Target="C:\Program Files\Microsoft Office\root\Office16\MSACCESS.EXE"},
+  @{Name="Excel"; Target="C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"},
+  @{Name="OneNote"; Target="C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"},
+  @{Name="Outlook"; Target="C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"},
+  @{Name="PowerPoint"; Target="C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE"},
+  @{Name="Publisher"; Target="C:\Program Files\Microsoft Office\root\Office16\MSPUB.EXE"},
+  @{Name="Word"; Target="C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"},
+  @{Name="Database Compare"; Target="C:\Program Files\Microsoft Office\root\Client\AppVLP.exe"; Arguments="`"C:\Program Files (x86)\Microsoft Office\Office16\DCF\DATABASECOMPARE.EXE`""; SystemLnk="Microsoft Office Tools\"},
+  @{Name="Office Language Preferences"; Target="C:\Program Files\Microsoft Office\root\Office16\SETLANG.EXE"; SystemLnk="Microsoft Office Tools\"},
+  @{Name="Spreadsheet Compare"; Target="C:\Program Files\Microsoft Office\root\Client\AppVLP.exe" ; Arguments="`"C:\Program Files (x86)\Microsoft Office\Office16\DCF\SPREADSHEETCOMPARE.EXE`""; SystemLnk="Microsoft Office Tools\"},
+  @{Name="Telemetry Log for Office"; Target="C:\Program Files\Microsoft Office\root\Office16\msoev.exe"; SystemLnk="Microsoft Office Tools\"},
+#  @{Name=""; Target=""; Arguments=""; SystemLnk=""; StartIn=""; Description=""}
 )
 
 for ($i = 0; $i -lt $sysAppList.length; $i++) {
   $app = $sysAppList[$i]
   $aName = $app.Name
-  $aSystemLnk = $app.SystemLnk
   $aTarget = $app.Target
   $aArguments = $app.Arguments
-  $aStartIn = if ($app.StartIn) {$app.StartIn} else {''}
+  $aSystemLnk = $app.SystemLnk
+  $aStartIn = if ($app.StartIn) {$app.StartIn} else {""}
+  $aDescription = $app.Description
 
-  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn
+  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn $aDescription
 }
 
 # OEM System Applications (e.g. Dell)
 
-<# $oemSysAppList = @(
-  @{Name=''; Target=''; Arguments=''; SystemLnk=''; StartIn=''}
+$oemSysAppList = @(
+  @{Name="Dell OS Recovery Tool"; Target="C:\Program Files (x86)\Dell\OS Recovery Tool\DellOSRecoveryTool.exe"; SystemLnk="Dell\"},
+  @{Name="SupportAssist Recovery Assistant"; Target="C:\Program Files\Dell\SARemediation\postosri\osrecoveryagent.exe"; SystemLnk="Dell\SupportAssist\"}
+#  @{Name=""; Target=""; Arguments=""; SystemLnk=""; StartIn=""; Description=""}
 )
 
 for ($i = 0; $i -lt $oemSysAppList.length; $i++) {
@@ -84,26 +97,34 @@ for ($i = 0; $i -lt $oemSysAppList.length; $i++) {
   $aSystemLnk = $app.SystemLnk
   $aTarget = $app.Target
   $aArguments = $app.Arguments
-  $aStartIn = if ($app.StartIn) {$app.StartIn} else {''}
+  $aStartIn = if ($app.StartIn) {$app.StartIn} else {""}
+  $aDescription = $app.Description
 
-  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn
-} #>
+  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn $aDescription
+}
 
 # Third-Party System Applications (not made by Microsoft)
 
 $sys3rdPartyAppList = @(
-  @{Name='Google Chrome'; Target='C:\Program Files\Google\Chrome\Application\chrome.exe'},
-  @{Name='Google Chrome (32-bit)'; Target='C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'},
-  @{Name='Firefox'; Target='C:\Program Files\Mozilla Firefox\firefox.exe'},
-  @{Name='Global VPN Client'; Target='C:\Program Files\SonicWALL\Global VPN Client\SWGVC.exe'},
-  @{Name='OpenVPN'; SystemLnk='OpenVPN\OpenVPN GUI'; Target='C:\Program Files\OpenVPN\bin\openvpn-gui.exe'},
-  @{Name='Adobe Acrobat'; Target='C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe'},
-  @{Name='Adobe Acrobat (32-bit)'; Target='C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\Acrobat.exe'},
-  @{Name='Epson Scan 2'; SystemLnk='EPSON\Epson Scan 2\Epson Scan 2'; Target='C:\Program Files (x86)\epson\Epson Scan 2\Core\es2launcher.exe'},
-  @{Name='FAX Utility'; SystemLnk='EPSON Software\FAX Utility'; Target='C:\Program Files (x86)\Epson Software\FAX Utility\FUFAXCNT.exe'},
-  @{Name='Altair Monarch 2020'; SystemLnk='Altair Monarch 2020\Altair Monarch 2020'; Target='C:\Program Files\Altair Monarch 2020\DWMonarch.exe'},
-  @{Name='USB Redirector TS Edition - Workstation'; SystemLnk='USB Redirector TS Edition - Workstation\USB Redirector TS Edition - Workstation'; Target='C:\Program Files\USB Redirector TS Edition - Workstation\usbredirectortsw.exe'}
-#  @{Name=''; Target=''; Arguments=''; SystemLnk=''; StartIn=''}
+  @{Name="Google Chrome"; Target="C:\Program Files\Google\Chrome\Application\chrome.exe"},
+  @{Name="Google Chrome (32-bit)"; Target="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"},
+  @{Name="Firefox"; Target="C:\Program Files\Mozilla Firefox\firefox.exe"},
+  @{Name="Firefox (32-bit)"; Target="C:\Program Files (x86)\Mozilla Firefox\firefox.exe"},
+  @{Name="Adobe Acrobat"; Target="C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"},
+  @{Name="Adobe Acrobat (32-bit)"; Target="C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\Acrobat.exe"},
+  @{Name="Global VPN Client"; Target="C:\Program Files\SonicWALL\Global VPN Client\SWGVC.exe"},
+  @{Name="OpenVPN"; Target="C:\Program Files\OpenVPN\bin\openvpn-gui.exe"; SystemLnk="OpenVPN\OpenVPN GUI"},
+  @{Name="CodeTwo Active Directory Photos"; Target="C:\Program Files\CodeTwo\CodeTwo Active Directory Photos\CodeTwo Active Directory Photos.exe"; SystemLnk="CodeTwo\CodeTwo Active Directory Photos\"},
+  @{Name="Go to program home page"; Target="C:\Program Files\CodeTwo\CodeTwo Active Directory Photos\Data\HomePage.url"; SystemLnk="CodeTwo\CodeTwo Active Directory Photos\"},
+  @{Name="User's manual"; Target="C:\Program Files\CodeTwo\CodeTwo Active Directory Photos\Data\User's manual.url"; SystemLnk="CodeTwo\CodeTwo Active Directory Photos\";},
+  @{Name="LAPS UI"; Target="C:\Program Files\LAPS\AdmPwd.UI.exe"; SystemLnk="LAPS\"},
+  @{Name="Microsoft Intune Management Extension"; Target="C:\Program Files (x86)\Microsoft Intune Management Extension\AgentExecutor.exe"; SystemLnk="Microsoft Intune Management Extension\"},
+#  @{Name=""; Target=""; Arguments=""; SystemLnk=""; StartIn=""; Description=""},
+  @{Name="Epson Scan 2"; Target="C:\Program Files (x86)\epson\Epson Scan 2\Core\es2launcher.exe"; SystemLnk="EPSON\Epson Scan 2\"},
+  @{Name="FAX Utility"; Target="C:\Program Files (x86)\Epson Software\FAX Utility\FUFAXCNT.exe"; SystemLnk="EPSON Software\"},
+  @{Name="Altair Monarch 2020"; Target="C:\Program Files\Altair Monarch 2020\DWMonarch.exe"; SystemLnk="Altair Monarch 2020\"},
+  @{Name="USB Redirector TS Edition - Workstation"; Target="C:\Program Files\USB Redirector TS Edition - Workstation\usbredirectortsw.exe"; SystemLnk="USB Redirector TS Edition - Workstation\"}
+#  @{Name=""; Target=""; Arguments=""; SystemLnk=""; StartIn=""; Description=""}
 )
 
 for ($i = 0; $i -lt $sys3rdPartyAppList.length; $i++) {
@@ -112,38 +133,41 @@ for ($i = 0; $i -lt $sys3rdPartyAppList.length; $i++) {
   $aSystemLnk = $app.SystemLnk
   $aTarget = $app.Target
   $aArguments = $app.Arguments
-  $aStartIn = if ($app.StartIn) {$app.StartIn} else {''}
+  $aStartIn = if ($app.StartIn) {$app.StartIn} else {""}
+  $aDescription = $app.Description
 
-  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn
+  Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn $aDescription
 }
 
 # User Applications (per user installed apps)
 
-$userAppList = @( # all instances of '%username%' get's replaced with the username
-  @{Name='OneDrive'; Target='C:\Users\%username%\AppData\Local\Microsoft\OneDrive\OneDrive.exe'},
-  @{Name='Microsoft Teams'; Target='C:\Users\%username%\AppData\Local\Microsoft\Teams\Update.exe'; Arguments='--processStart "Teams.exe"'},
-  @{Name='Google Chrome'; Target='C:\Users\%username%\AppData\Local\Google\Chrome\Application\chrome.exe'},
-  @{Name='Firefox'; Target='C:\Users\%username%\AppData\Local\Mozilla Firefox\firefox.exe'},
-  @{Name='RingCentral'; Target='C:\Users\%username%\AppData\Local\Programs\RingCentral\RingCentral.exe'},
-  @{Name='RingCentral Meetings'; SystemLnk='RingCentral Meetings\RingCentral Meetings'; Target='C:\Users\%username%\AppData\Roaming\RingCentralMeetings\bin\RingCentralMeetings.exe'},
-  @{Name='Uninstall RingCentral Meetings'; SystemLnk='RingCentral Meetings\Uninstall RingCentral Meetings'; Target='C:\Users\Andrew\AppData\Roaming\RingCentralMeetings\uninstall\Installer.exe'; Arguments='/uninstall'}
-#  @{Name=''; Target=''; Arguments=''; SystemLnk=''; StartIn=''}
+$userAppList = @( # all instances of "%username%" get's replaced with the username
+  @{Name="OneDrive"; Target="C:\Users\%username%\AppData\Local\Microsoft\OneDrive\OneDrive.exe"},
+  @{Name="Microsoft Teams"; Target="C:\Users\%username%\AppData\Local\Microsoft\Teams\Update.exe"; Arguments="--processStart `"Teams.exe`""},
+  @{Name="Google Chrome"; Target="C:\Users\%username%\AppData\Local\Google\Chrome\Application\chrome.exe"},
+  @{Name="Firefox"; Target="C:\Users\%username%\AppData\Local\Mozilla Firefox\firefox.exe"},
+  @{Name="RingCentral"; Target="C:\Users\%username%\AppData\Local\Programs\RingCentral\RingCentral.exe"},
+  @{Name="RingCentral Meetings"; Target="C:\Users\%username%\AppData\Roaming\RingCentralMeetings\bin\RingCentralMeetings.exe"; SystemLnk="RingCentral Meetings\"},
+  @{Name="Uninstall RingCentral Meetings"; Target="C:\Users\Andrew\AppData\Roaming\RingCentralMeetings\uninstall\Installer.exe"; Arguments="/uninstall"; SystemLnk="RingCentral Meetings\"}
+#  @{Name=""; Target=""; Arguments=""; SystemLnk=""; StartIn=""; Description=""}
 )
 
 # get all users 
-$Users = (Get-ChildItem 'C:\Users\' | % { $_.name })
+$Users = (Get-ChildItem "C:\Users\" | % { $_.name })
 if ($Users[0].length -eq 1) {$Users = @("$Users")} # if only one user, array needs to be recreated
 
 for ($i = 0; $i -lt $userAppList.length; $i++) {
   $app = $userAppList[$i]
   $aName = $app.Name
   $aArguments = $app.Arguments
-  $aStartIn = if ($app.StartIn) {$app.StartIn} else {''}
+  $aStartIn = if ($app.StartIn) {$app.StartIn} else {""}
+  $aDescription = $app.Description
 
   for ($j = 0; $j -lt $Users.length; $j++) {
     $aUser = $Users[$j]
+    $aTarget = ($app.Target).replace("%username%", $aUser)
     $aSystemLnk = ("C:\Users\${aUser}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"+$app.SystemLnk)
-    $aTarget = ($app.Target).replace('%username%', $aUser)
-    Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn
+
+    Recreate-Shortcut $aName $aTarget $sArguments $aSystemLnk $aStartIn $aDescription
   }
 }
