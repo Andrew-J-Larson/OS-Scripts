383adcdd7871eff9eb5a5cd15d84ddb575e47ed8
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2023-01-15 12:38:31 -0800

    Clarification of Acrobat version

--- Windows/Microsoft-Endpoint-Defender/Recreate-Base-Shortcuts.ps1
+++ Windows/Microsoft-Endpoint-Defender/Recreate-Base-Shortcuts.ps1
@@ -377,10 +377,10 @@ $sys3rdPartyAppList = @(
   @{Name="Adobe Acrobat Distiller"; TargetPath="C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodist.exe"},
   @{Name="Adobe Acrobat (32-bit)"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\Acrobat.exe"},
   @{Name="Adobe Acrobat Distiller (32-bit)"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\acrodist.exe"},
-  @{Name="Adobe Acrobat Reader"; TargetPath="C:\Program Files\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"}, # it's the only install on 32-bit
-  @{Name="Adobe Acrobat Distiller"; TargetPath="C:\Program Files\Adobe\Acrobat Reader DC\Reader\acrodist.exe"}, # it's the only install on 32-bit
-  @{Name="Adobe Acrobat Reader"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"}, # it's the only install on 64-bit
-  @{Name="Adobe Acrobat Distiller"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\acrodist.exe"}, # it's the only install on 64-bit
+  @{Name="Adobe Acrobat Reader"; TargetPath="C:\Program Files\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"}, # old version; it's the only install on 32-bit
+  @{Name="Adobe Acrobat Distiller"; TargetPath="C:\Program Files\Adobe\Acrobat Reader DC\Reader\acrodist.exe"}, # old version; it's the only install on 32-bit
+  @{Name="Adobe Acrobat Reader"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"}, # old version; it's the only install on 64-bit
+  @{Name="Adobe Acrobat Distiller"; TargetPath="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\acrodist.exe"}, # old version; it's the only install on 64-bit
   # Altair Monarch
   @{Name="Altair Monarch 2021"; TargetPath="C:\Program Files\Altair Monarch 2021\DWMonarch.exe"; SystemLnk="Altair Monarch 2021\"},
   @{Name="Altair Monarch 2021 (32-bit)"; TargetPath="C:\Program Files (x86)\Altair Monarch 2021\DWMonarch.exe"; SystemLnk="Altair Monarch 2021\"},
