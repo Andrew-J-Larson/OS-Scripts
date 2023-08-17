a3a347d03335f6407fcda3f59943b0dea95382bb
Drew <18andrew.j.l+github@gmail.com>
2023-08-17 11:16:44 -0700

    Fixed email
    More professional

--- Mac/Change-Lockscreen/Source Files/2-shellscript.sh
+++ Mac/Change-Lockscreen/Source Files/2-shellscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2023  Andrew Larson (thealiendrew@gmail.com)
+# Copyright (C) 2023  Andrew Larson (andrew.j.larson18+github@gmail.com)
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
--- Mac/MacPorts/installer.sh
+++ Mac/MacPorts/installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Auto installs the MacPorts package manager.
 
-# Copyright (C) 2023  Andrew Larson (thealiendrew@gmail.com)
+# Copyright (C) 2023  Andrew Larson (andrew.j.larson18+github@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
