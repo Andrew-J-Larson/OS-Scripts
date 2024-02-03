dde2d9ffffce12abbabe92824f2f358e9f346ffd
Drew <andrew.j.larson18+github@gmail.com>
2024-02-03 08:21:44 -0800

    Update MediaCreationTool_Run_Preset.ps1
    Using CIM instead of deprecated WMI

--- Windows/MediaCreationTool/MediaCreationTool_Run_Preset.ps1
+++ Windows/MediaCreationTool/MediaCreationTool_Run_Preset.ps1
@@ -1,6 +1,6 @@
 <#
   .SYNOPSIS
-  MediaCreationTool Run Preset v1.0.5
+  MediaCreationTool Run Preset v1.0.6
 
   .DESCRIPTION
   Script helps to automate a part of the process needed to generate single edition ISOs.
@@ -384,9 +384,9 @@ $WIN_VERSION_MCT.win11.timestampFile = $WIN_VERSION_MCT.win11.folder + "\timesta
 #  1. Current logged in user is an admin
 #  2. The program is elevated
 #  3. Only one instance of program is started
-$loggedOnUser = (Get-WMIObject -class Win32_ComputerSystem).Username
+$loggedOnUser = (Get-CimInstance Win32_ComputerSystem).Username
 $elevatedUser = [Security.Principal.WindowsIdentity]::GetCurrent().Name
-$wdagUtilUser = (Get-WMIObject -class Win32_ComputerSystem).Name + '\WDAGUtilityAccount'
+$wdagUtilUser = (Get-CimInstance Win32_ComputerSystem).Name + '\WDAGUtilityAccount'
 if (-Not (($loggedOnUser -eq $elevatedUser) -or ($wdagUtilUser -eq $elevatedUser))) {
     Add-Type -AssemblyName Microsoft.VisualBasic -ErrorAction SilentlyContinue
     [Microsoft.VisualBasic.Interaction]::MsgBox('Must be signed in as an admin, or ran inside Windows Sandbox, to use this tool.', 'OKOnly,SystemModal,Information', $MyInvocation.MyCommand.Name) | Out-Null
